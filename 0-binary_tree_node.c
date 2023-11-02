7a63ab83526d018ab07d6d3f9377beaacbe9beb4
Oluwasegun Amuda <ademilolaamuda@gmail.com>
2023-11-02 09:24:35 +0100

    some corrections and amendments

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node.
- * @parent: A pointer to the parent of the node to create.
- * @value: The value to put in the new node.
+ * binary_tree_node - This function creates a node for binaryTree.
+ * @parent: Pointer to the initial of the node to create.
+ * @value: This reps the value to put in the node created.
  *
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a node as a left-child of
- *                           of another in a binary tree.
- * @parent: A pointer to the node to insert the left-child in.
- * @value: The value to store in the new node.
+ * binary_tree_insert_left - This function inserts a another
+ * node as a left-child of another in a binary tree.
+ * @parent: This is a pointer to the node to insert the
+ * left-child into.
+ * @value: Value to store in the new node.
  *
  * Return: If parent is NULL or an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
